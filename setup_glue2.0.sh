6c611368e9393ac4ef6d237a485abf6f0af6cbf8
崔起鎮 <choi.kijin@techcross.co.jp>
2022-02-09 09:42:26 +0900

    added script and config for local usage

---
+++ setup_glue2.0.sh
@@ -0,0 +1,6 @@
+# install maven3.6.3
+curl -O https://aws-glue-etl-artifacts.s3.amazonaws.com/glue-common/apache-maven-3.6.0-bin.tar.gz
+tar zxf apache-maven-3.6.0-bin.tar.gz
+# install sppark
+curl -O https://aws-glue-etl-artifacts.s3.amazonaws.com/glue-2.0/spark-2.4.3-bin-hadoop2.8.tgz
+tar zxf spark-2.4.3-bin-hadoop2.8.tgz
